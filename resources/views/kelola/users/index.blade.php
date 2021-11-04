1306eeba8e9b64fe33ba7cef223df43c978724aa
tickernelz <zhafronadani@gmail.com>
2021-11-04 20:57:59 +0700

    tambah vendor plugins
    Signed-off-by: tickernelz <zhafronadani@gmail.com>

---
+++ resources/views/kelola/users/index.blade.php
@@ -0,0 +1,51 @@
+@extends('adminlte::page')
+
+@section('title', 'List Pengguna')
+
+@section('content_header')
+    <h1>List Pengguna</h1>
+@stop
+
+@php
+    $heads = [
+        '#',
+        'NIP',
+        'Username',
+        'Nama',
+        'Peran',
+        'Aksi',
+    ];
+
+$config = [
+    'order' => [[0, 'asc']],
+    'columns' => [null, null, null, null, null, ['orderable' => false, 'className' => 'text-center']],
+];
+@endphp
+
+@section('content')
+    <x-adminlte-datatable id="table" :config="$config" :heads="$heads" striped hoverable bordered>
+        @foreach($data as $li)
+            <tr>
+                <td>{!! $loop->iteration !!}</td>
+                <td>{!! $li->nip !!}</td>
+                <td>{!! $li->username !!}</td>
+                <td>{!! $li->nama !!}</td>
+                <td>{!! $li->roles->first()->name !!}</td>
+                <td>
+                    <div class="btn-group btn-group-sm" role="group">
+                        <a type="button" class="btn btn-secondary"
+                           href="{{ Request::url() }}/edit/{{$li->id}}">
+                            <i class="fa fa-edit"></i>
+                        </a>
+                        <a type="button" class="btn btn-secondary"
+                           href="{{ Request::url() }}/hapus/{{$li->id}}"
+                           onclick="return confirm('Yakin Mau Dihapus?');">
+                            <i class="fa fa-trash"></i>
+                        </a>
+                    </div>
+                </td>
+            </tr>
+        @endforeach
+    </x-adminlte-datatable>
+@stop
+
